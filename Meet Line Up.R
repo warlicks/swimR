1d04f4baae730f3c234ef69dd25a0cea78808fdc
warlicks <warlick.sean@gmail.com>
2015-11-18 21:29:25 -0500

    Started developing code to score swim meet.

---
+++ Meet Line Up.R
@@ -0,0 +1,52 @@
+library(ggvis)
+
+
+home_team <- "IOWA"
+visiting_team <- "USC"
+meet_gender <- "M"
+
+
+# Create Data Set For the Swim Meet
+###############################################################################
+meet_data <- master_times %>% 
+ filter(team_code %in% c(home_team, visiting_team) & gender == meet_gender) %>%
+ 	# To make the data set manageable we will filter to just the two teams competing and the appropriate gender
+  mutate(event_count = 0)
+  	# We will initilize a variable to keep track of how many events an athlete has swam
+	
+# Scoring Rules
+###############################################################################
+# Scoring Rules are based on 2015-16 and 2016-17 NCAA Rules for dual meets  with 6 or more lanes. 
+
+place<-c(1, 2, 3, 4, 5, 6)
+points<-c(9, 4, 3, 2, 1, 0)
+
+scoring_rules<-as.data.frame(cbind(place, points))
+
+
+
+test_events <- c("50 Freestyle", "100 Freestyle") # U
+
+meet_data2 <- meet_data %>%
+	filter(swimmer_event_rank <= 3 & event_count <= 3 
+		& event == test_events[i]) %>%
+	group_by(team_code) %>%
+	mutate(team_event_rank2 = rank(swim_time)) %>%
+	filter(team_event_rank2 <= 3) %>%
+	select(full_name_computed, team_short_name, team_code, swim_time, swim_time2, team_event_rank2)
+
+meet_data2 <- meet_data2 %>%
+	ungroup() %>%
+	mutate(predicted_finish = rank(swim_time)) %>%
+	inner_join(scoring_rules, by = c("predicted_finish" = "place"))
+	
+
+event_score <- meet_data2 %>%
+	group_by(team_code) %>%
+	mutate(event_score = sum(points)) %>%
+	distinct(team_code) %>%
+	select(team_code, event_score)
+
+event_score %>% 
+	ggvis(~team_code, ~event_score) %>% 
+	layer_bars()
