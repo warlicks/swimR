0c9c367327adf26fb199a593052e5b00ef0d8832
warlicks <warlick.sean@gmail.com>
2016-09-07 22:21:13 -0700

    Updated file handling, so file is correctly renamed.  Added option to read file into memory after download.  Start to implment ability to set date range.

--- Data Collection.R
+++ Data Collection.R
@@ -4,15 +4,19 @@
 # from USA Swimming's Website. 
 ###############################################################################
 
+#'
+#'
+#'
 
 
-
-individual_swims<-function(Conference, LCM = FALSE, SCM = FALSE, SCY = TRUE, top){
+individual_swims<-function(Conference, start_date = '2015-08-01', end_date = '2015-05-31', LCM = FALSE, SCM = FALSE, SCY = TRUE, top, downloadPath = getwd(), read = TRUE){
 	require(RSelenium) # Rselenium Provides Tools Fill Out Search Form. 
 
 	url<-"http://usaswimming.org/DesktopDefault.aspx?TabId=1971&Alias=Rainbow&Lang=en"
 	
 	selserv <- startServer(args = c("-port 4445")) # Star Selenium Server
+
+	fprof <- profile(downloadPath)
 
 	remDr <- remoteDriver(remoteServerAddr = 'localhost', port = 4445, browser = "firefox", extraCapabilities = fprof)
 
@@ -23,14 +27,14 @@ individual_swims<-function(Conference, LCM = FALSE, SCM = FALSE, SCY = TRUE, top
 	time_type <- remDr$findElement(using = "id", value = "ctl82_rbIndividual")
 	time_type$clickElement() #Make Sure Indivual Times are Selected
 
-	conference_select <- key_press(Conference)
+	# Select Conference
+	conference_select <- key_press(Conference) # Set Up Key Strokes
 
 	conference<-remDr$findElement(using = "id", value = "ctl82_ddConference")
-	conference$sendKeysToElement(conference_select)
+	conference$sendKeysToElement(conference_select) # Pass Key Storkes
 
 
 	# Select The Courses
-	# NEED TO MODIFY THIS IF PLACED IN LOOP SO THAT THEY DON'T GET TURNED BACK ON
 	if(LCM == FALSE){
 		lcm<-remDr$findElement(using = "id", value = "ctl82_cblCourses_0")
 		lcm$clickElement()
@@ -42,9 +46,18 @@ individual_swims<-function(Conference, LCM = FALSE, SCM = FALSE, SCY = TRUE, top
 	} # Uncheck SCM Button
 
 	if(SCY == FALSE){
-		scy<-remDr$findElement(using = "id", value = "ctl82_cblCourses_2")
+		scy <- remDr$findElement(using = "id", value = "ctl82_cblCourses_2")
 		scy$clickElement()	
 	} # Uncheck SCY Button
+
+	# Set Date Range
+	date_button <- remDr$findElement(using = "id", value = "ctl82_rbDateRange")
+	date_button$clickElement()
+
+	## Start Date
+	start_dateBox <- remDr$findElement(using = "id", value = "ctl00_ctl82_ucStartDate_radTheDate_dateInput")
+	start_date <- startDateParse(start_date)
+	start_dateBox$sendKeysToElement(start_date)
 
 	# Select How Many Times To Display
 	if(top > 500){
@@ -56,17 +69,10 @@ individual_swims<-function(Conference, LCM = FALSE, SCM = FALSE, SCY = TRUE, top
 	show_top$clearElement()
 	show_top$sendKeysToElement(list(as.character(top)))
 
-	# Turn Off Altitude Adjustment
-	#if(){
-	#	altitude<-remDr$findElement(using = "id", value = "ctl82_cbUseAltitudeAdjTime")
-	#	altitude$clickElement()
-	#} This Will Be USED WHEN I CREATE LOOP TO PULL ALL CONFERENCES
-
 	altitude <- remDr$findElement(using = "id", value = "ctl82_cbUseAltitudeAdjTime")
 	altitude$clickElement()
 
-	#output_select <- remDr$findElement(using = "id", value = "ctl82_ddOutputType")
-	#output_select$sendKeysToElement(list('R'))
+
 
 	search <- remDr$findElement(using = "id", value = "ctl82_btnCreateReport")
 	search$clickElement()
@@ -83,25 +89,30 @@ individual_swims<-function(Conference, LCM = FALSE, SCM = FALSE, SCY = TRUE, top
 	output_select$sendKeysToElement(list("E", "E", "E"))
 	change_output <- remDr$findElement(using = "id", value = "ctl82_ucReportViewer_lbChangeOutputType" )
 	
-	Sys.sleep(10) # Need to pause to get the export click to work
+	Sys.sleep(5) # Need to pause to get the export click to work
 
 	change_output$clickElement()
 
 
 	Sys.sleep(30) # Give Time for Down Load
-
-
-	# # File Management
-	# file <- list.files(path = "/Users/SeanWarlick/Downloads", pattern = "*.csv")
-
-	# file.rename(file, paste(Conference, ".csv", sep = "")) #Give Meaningful Name
-
-
 	remDr$close()
 
-	# #Combine To One File
-	# setwd("")
-	# list.files()
+	# File Management ----
+	currentwd <- getwd()
+	setwd(downloadPath)
+	
+	file <- file.info(dir()) # Data Frame Used to find downloaded file.
+
+	file_name <- row.names(file)[which(file$ctime == max(file$ctime))] # Use create time find name of downloaded file
+	
+	file.rename(file_name, paste(Conference, ".csv", sep = "")) #Give Meaningful Name
+
+	if(read){
+		data <- read.csv(paste(Conference, ".csv", sep = ""))
+		return(data)
+	}
+
+	setwd(currentwd)
 
 }
 
