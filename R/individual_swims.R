61be137d6435df2ab50a1c314330cc5c423bf3a7
warlicks <warlick.sean@gmail.com>
2016-09-10 12:27:32 -0700

    Fixed bugs.  Function now runs without errors.

--- R/individual_swims.R
+++ R/individual_swims.R
@@ -15,8 +15,10 @@
 #' @param SCM logical.  Optional. Should short course meter events be included in the report?  Defaults to FALSE.
 #' @param SCY logical.  Optional. Should short course yard events be included in the report?  Defaults to TRUE.
 #' @param top integer. How many swimmers should be returned in each event? Maximum allowed is 500. 
-#' @param
-
+#' @param downloadPath character sting
+#' @param read logical
+#'
+#' @export
 
 
 individual_swims<-function(Conference, start_date = '2015-08-01', end_date = '2015-05-31', LCM = FALSE, SCM = FALSE, SCY = TRUE, top, downloadPath = getwd(), read = TRUE){
@@ -60,7 +62,7 @@ individual_swims<-function(Conference, start_date = '2015-08-01', end_date = '20
 		scy$clickElement()	
 	} # Uncheck SCY Button
 
-	# Set Date Range
+	# Set Date Range ----
 	date_button <- remDr$findElement(using = "id", value = "ctl82_rbDateRange")
 	date_button$clickElement()
 
@@ -69,35 +71,32 @@ individual_swims<-function(Conference, start_date = '2015-08-01', end_date = '20
 	start_date <- start_dateParse(start_date)
 	start_dateBox$sendKeysToElement(start_date)
 
+	## End Date
 	end_dateBox <- remDr$findElement(using = "id", value = "ctl00_ctl82_ucEndDate_radTheDate_dateInput")
-	end_date <- endDateParse(end_date)
+	end_date <- end_dateParse(end_date)
+	end_dateBox$sendKeysToElement(end_date)
 
-	# Select How Many Times To Display
+	# Select How Many Times To Display ----
+	## Error Check
 	if(top > 500){
 		warning("Can Not Excede Top 500 Times")
-		stop()
-	} # Create Warning and Stop Execution if we excede USA Swimmings Max Value
+		stop() # Create Warning and Stop if we excede USA Swimmings Max Value
+	} 
 
+	## Select top number of time.
 	show_top <- remDr$findElement(using = "id", value = "ctl82_txtShowTopX")
 	show_top$clearElement()
 	show_top$sendKeysToElement(list(as.character(top)))
 
+	# Trun off altitude adjustment ----
 	altitude <- remDr$findElement(using = "id", value = "ctl82_cbUseAltitudeAdjTime")
 	altitude$clickElement()
 
-
-
+	# Submit report search ----
 	search <- remDr$findElement(using = "id", value = "ctl82_btnCreateReport")
 	search$clickElement()
     
-
- #    Sys.sleep(20) # Need to pause to get the export click to work
-	
-	# report_frame <- remDr$findElement(using = "tag name", value = "iframe")
-
-	# report_frame$click(buttonId = "RIGHT")
-
-	# Change The Output To CSV & Save!
+	# Change The Output To CSV & Save! ----
 	output_select <- remDr$findElement(using = "id", value = "ctl82_ucReportViewer_ddViewerType")
 	output_select$sendKeysToElement(list("E", "E", "E"))
 	change_output <- remDr$findElement(using = "id", value = "ctl82_ucReportViewer_lbChangeOutputType" )
@@ -106,11 +105,11 @@ individual_swims<-function(Conference, start_date = '2015-08-01', end_date = '20
 
 	change_output$clickElement()
 
-
 	Sys.sleep(30) # Give Time for Down Load
 	remDr$close()
-
+	
 	# File Management ----
+	## If working directory is different from the download path. 
 	currentwd <- getwd()
 	setwd(downloadPath)
 	
@@ -125,7 +124,7 @@ individual_swims<-function(Conference, start_date = '2015-08-01', end_date = '20
 		return(data)
 	}
 
-	setwd(currentwd)
+	setwd(currentwd) # Return to working directory
 
 }
 
