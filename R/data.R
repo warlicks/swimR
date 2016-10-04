0a3b8ca556fb1cec89cbd11caaca7b8d16a24dec
warlicks <warlick.sean@gmail.com>
2016-10-03 20:28:44 -0700

    Created data sets with qualifying times and started to document the data sets.

---
+++ R/data.R
@@ -0,0 +1,50 @@
+#' NCAA Qualifying Times
+#'
+#' A Dataset containing the qualifying times for the 2017 NCAA Division I 
+#' Swimming Championships.
+#' 
+#' @format A data frame with 52 rows and 5 columns:
+#' \describe{
+#'	\item{event}{}
+#'	\item{gender}{}
+#'	\item{standard}{}
+#'  \item{swim_time}{}
+#'  \item{swim_time2}{}
+#' }
+#'
+#' @source \url{https://swimswam.com/ncaa-releases-2016-2017-division-time-standards/}  
+"D1Qualifying"
+
+#' NCAA Qualifying Times
+#'
+#' A Dataset containing the qualifying times for the 2017 NCAA Division II 
+#' Swimming Championships.
+#' 
+#' @format A data frame with 52 rows and 5 columns:
+#' \describe{
+#'	\item{event}{}
+#'	\item{gender}{}
+#'	\item{standard}{}
+#'  \item{swim_time}{}
+#'  \item{swim_time2}{}
+#' }
+#'
+#' @source \url{https://swimswam.com/ncaa-division-ii-time-standards-released/}  
+"D2Qualifying"
+
+#' NCAA Qualifying Times
+#'
+#' A Dataset containing the qualifying times for the 2017 NCAA Division II 
+#' Swimming Championships.
+#' 
+#' @format A data frame with 52 rows and 5 columns:
+#' \describe{
+#'	\item{event}{}
+#'	\item{gender}{}
+#'	\item{standard}{}
+#'  \item{swim_time}{}
+#'  \item{swim_time2}{}
+#' }
+#'
+#' @source \url{https://swimswam.com/ncaa-releases-division-iii-time-standards/}  
+"D3Qualifying"
