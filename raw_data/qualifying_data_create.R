f6ec7d44057006937d3d5586f5f7f3fc9315e2d4
warlicks <warlick.sean@gmail.com>
2016-10-03 20:31:50 -0700

    Added code used to generate qualifying time data sets.

---
+++ raw_data/qualifying_data_create.R
@@ -0,0 +1,16 @@
+library(devtools)
+
+source("~/Documents/GitHub/swimR/R/cut_times.R")
+source("~/Documents/GitHub/swimR/R/standard_clean.R")
+
+D1Qualifying <- cut_times('https://swimswam.com/ncaa-releases-2016-2017-division-time-standards/')
+
+D2Qualifying <- cut_times('https://swimswam.com/ncaa-division-ii-time-standards-released/', division = 2)
+
+D3Qualifying <- cut_times('https://swimswam.com/ncaa-releases-division-iii-time-standards/', division = 3)
+
+setwd("~/Documents/GitHub/swimR")
+
+use_data(D1Qualifying)
+use_data(D2Qualifying)
+use_data(D3Qualifying)
