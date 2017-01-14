0ececf7113902ae00349439f4769e35c36afff05
warlicks <warlick.sean@gmail.com>
2017-01-13 19:28:13 -0800

    Changed param for swim_db_table. Now we create connection outside the functional call.  Fixed bugs in the SQL used to create tables. Function now tells user that table was correctly created with message in the terminal

--- R/createTables.R
+++ R/createTables.R
@@ -6,7 +6,7 @@
 
 #' Code to create each table in the database. 
 #'
-#' @parm con is a database connection object of class \link[DBI]{
+#' @param con is a database connection object of class \link[DBI]{
 #' DBIConnection-class}
 #'
 #' @keywords internal
@@ -19,25 +19,30 @@ create_team <- function(con){
 			ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 			CONFERENCE_ID INTEGER,
 			TEAM_CODE Varchar(5) NOT NULL UNIQUE, 
-			TEAMEAM_NAME VARCHAR(50) NOT NULL UNIQUE
+			TEAM_NAME VARCHAR(50) NOT NULL UNIQUE
 		)
 	"
 	# Execute the statement
 	if(DBI::dbExistsTable(con, 'TEAM')) {
 		print('Team Table Already Built')
 	} else {
-		create <- DBI::dbSendStatement(con, statement)
+		create <- DBI::dbExecute(con, statement)
 
 		# Check That Create Table worked
 		if(DBI::dbExistsTable(con, 'TEAM') == FALSE) {
 			warning("Team Table Was Not Created")
+		} else {
+			print("TEAM Table Was Built")
 		}
 	}
+
+	#dbDisconnect(con)
+
 }
 
 #' Code to create each table in the database. 
 #'
-#' @parm con is a database connection object of class \link[DBI]{
+#' @param con is a database connection object of class \link[DBI]{
 #' DBIConnection-class}
 #'
 #' @keywords internal
@@ -47,26 +52,30 @@ create_conference <- function(con){
 	# Define the Create table statement 
 	statement <- "
 		CREATE TABLE CONFERENCE(
-			ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE
+			ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 			NAME VARCHAR(100)
 		)
 	"
 	# Execute the statement
 	if(DBI::dbExistsTable(con, 'CONFERENCE')){
-		print('Team Table Already Built')
+		print('Conference Table Already Built')
 	} else {
-		create <- DBI::dbSendStatement(con, statement)
+		create <- DBI::dbExecute(con, statement)
 
 		# Check That Create Table worked
 		if(DBI::dbExistsTable(con, 'CONFERENCE') == FALSE) {
-			warning("Team Table Was Not Created")
+			warning("CONFERENCE Table Was Not Created")
+		} else {
+			print("CONFERENCE Table Was Built")
 		}
 	}
+	#DBI::dbDisconnect(con)
+
 }
 
 #' Code to create each table in the database. 
 #'
-#' @parm con is a database connection object of class \link[DBI]{
+#' @param con is a database connection object of class \link[DBI]{
 #' DBIConnection-class}
 #'
 #' @keywords internal
@@ -75,7 +84,7 @@ create_conference <- function(con){
 create_athlete <- function(con){
 	# Define the Create table statement 
 	statement <- "
-		CREATE TABLE ATHLETE (
+		CREATE TABLE ATHLETE(
 			ID VARCHAR(15) NOT NULL PRIMARY KEY UNIQUE,
 			TEAM_ID INTEGER NOT NULL,
 			ATHLETE_NAME VARCHAR(50) NOT NULL,
@@ -87,18 +96,20 @@ create_athlete <- function(con){
 	if(DBI::dbExistsTable(con, 'ATHLETE')){
 		print('Athlete Table Already Built')
 	} else {
-		create <- DBI::dbSendStatement(con, statement)
+		create <- DBI::dbExecute(con, statement)
 		
 		# Check That Create Table worked
 		if(DBI::dbExistsTable(con, 'ATHLETE') == FALSE) {
 			warning("Athlete Table Was Not Created")
+		}else {
+			print("ATHLETE Table Was Built")
 		}
 	} 
 }
 
 #' Code to create each table in the database. 
 #'
-#' @parm con is a database connection object of class \link[DBI]{
+#' @param con is a database connection object of class \link[DBI]{
 #' DBIConnection-class}
 #'
 #' @keywords internal
@@ -108,7 +119,7 @@ create_meet <- function(con){
 	# Define the Create table statement 
 	statement <- "
 		CREATE TABLE MEET(
-			ID NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE
+			ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 			MEET_NAME VARCHAR(50) NOT NULL UNIQUE,
 			MEET_DATE CHAR(10) NOT NULL
 		)
@@ -117,11 +128,13 @@ create_meet <- function(con){
 	if(DBI::dbExistsTable(con, 'MEET')) {
 		print('Meet Table Already Built')
 	} else {
-		create <- DBI::dbSendStatement(con, statement)
+		create <- DBI::dbExecute(con, statement)
 		
 		# Check That Create Table worked
 		if(DBI::dbExistsTable(con, 'MEET') == FALSE) {
 			warning("Meet Table Was Not Created")
+		} else {
+			print("MEET Table Was Built")
 		}
 	} 
 }
@@ -129,7 +142,7 @@ create_meet <- function(con){
 
 #' Code to create each table in the database. 
 #'
-#' @parm con is a database connection object of class \link[DBI]{
+#' @param con is a database connection object of class \link[DBI]{
 #' DBIConnection-class}
 #'
 #' @keywords internal
@@ -139,7 +152,7 @@ create_event <- function(con){
 	# Define the Create table statement 
 	statement <- "
 		CREATE TABLE EVENT(
-			--ID INTEGER,
+			ID INTEGER,
 			EVENT CHAR(15) NOT NULL,
 			GENDER CHAR(2) NOT NULL,
 			STANDARD CHAR(18) NOT NULL,
@@ -150,11 +163,13 @@ create_event <- function(con){
 	if(DBI::dbExistsTable(con, 'EVENT')){
 		print('Event Table Already Built')
 	} else {
-		create <- DBI::dbSendStatement(con, statement)
+		create <- DBI::dbExecute(con, statement)
 		
 		# Check That Create Table worked
 		if(DBI::dbExistsTable(con, 'EVENT') == FALSE) {
 			warning("Event Table Was Not Created")
+		} else {
+			print("EVENT Table Was Built")
 		}
 	}
 }
@@ -162,7 +177,7 @@ create_event <- function(con){
 
 #' Code to create each table in the database. 
 #'
-#' @parm con is a database connection object of class \link[DBI]{
+#' @param con is a database connection object of class \link[DBI]{
 #' DBIConnection-class}
 #'
 #' @keywords internal
@@ -172,11 +187,11 @@ create_result <- function(con){
 	# Define the Create table statement 
 	statement <- "
 		CREATE TABLE RESULT(
-			ID NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE
+			ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 			MEET_ID NOT NULL, 
 			ATHLETE_ID VARCHAR(15) NOT NULL,
 			EVENT CHAR(15) NOT NULL,
-			SWIM_TIME_TEXT VARCHAR(10) NOT NULL
+			SWIM_TIME_TEXT VARCHAR(10) NOT NULL,
 			SWIM_TIME_VALUE NUMERIC NOT NULL	
 		)
 	"
@@ -184,11 +199,13 @@ create_result <- function(con){
 	if(DBI::dbExistsTable(con, 'RESULT')){
 		print('Result Table Already Built')
 	} else {
-		create <- DBI::dbSendStatement(con, statement)
+		create <- DBI::dbExecute(con, statement)
 		
 		# Check That Create Table worked
 		if(DBI::dbExistsTable(con, 'RESULT') == FALSE) {
 			warning("Result Table Was Not Created")
+		} else {
+			print("RESULT Table Was Built")
 		}
 	}
 }
--- R/swimdb_tables.R
+++ R/swimdb_tables.R
@@ -7,24 +7,19 @@
 #' 
 #' Create tables to store data in swimming database.
 #'
-#'\code{swimdb_tables} is used to create tables in a SQLite database.  These tables are designed to store results from USA Swimmings Top Times Report.  The function utilizes \code{\link[DBI]{DBI}} create the connection to the database, creation of the tables and insertion of the data.  
+#'\code{swim_db_tables} is used to create tables in a SQLite database.  These tables are designed to store results from USA Swimmings Top Times Report.  The function utilizes \code{\link[DBI]{DBI}} create the connection to the database, creation of the tables and insertion of the data.  
 #'
 #' If the database provided in /emph{db_name} does not exist the fuction will create the SQLite database.
 
-#' @param db_name A character string providing the filename of the SQLite Database.
+#' @param con A character string providing the name of a SQLiteConnection to the desired database.
 #'
 #' @export
 
-swimdb_tables <- function(db_name){
-
-	# Connect to SQLite DB.  If it doesn't exit it will be created.
-
-	driver <- RSQLite::SQLite()
-	con    <- DBI::dbConnect(driver, db_name)
+swim_db_tables <- function(con){
 
 	# Create the Tables
-	create_team(con)
 	create_conference(con)
+	create_team(con)
 	create_athlete(con)
 	create_meet(con)
 	create_event(con)
