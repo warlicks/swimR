b725942fd4be806e932ad575541e774509d93048
warlicks <warlick.sean@gmail.com>
2017-02-10 22:30:04 -0800

    Updated documentation

--- R/swim_db_data_load.R
+++ R/swim_db_data_load.R
@@ -3,8 +3,19 @@
 # Source Code for swimR
 # Internal Functions to load data into swim database
 ###############################################################################
-#' Load Data Frame Into Swim Database
+#' Load college swimming results into a database.  
 #'
+#' Load college swimming results into a database.  
+#'
+#' \code{swim_db_data_load()} allows users to easily load swimming results data into a  database.  It is specifically designed to load data from from USA Swimmings Top Times Report.  Conference data, team data, athlete data, meet data and result data is loaded into the database with the function.  
+#'
+#' The function is set up so only distinct data is stored.  If a the function encounters a duplicate record it skips the records and moves on to the next record.
+#'
+#' @param con A SQLiteConnection object. 
+#'
+#' @param data A data.frame containing swimming results.  The data.frame should follow the form of a USA Swimming Top Times report and been cleaned by \code{\link{clean_swim()}}.
+#'
+#' @param conference_name A character string providing the name of the conference repersented in /emph{data}.   
 #'
 #' @export
 swim_db_data_load <- function(con, data, conference_name){
