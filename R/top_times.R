1b8d689d1f643a35a161497355ba99836e0f410d
warlicks <warlick.sean@gmail.com>
2017-03-11 18:12:15 -0800

    implemented procedures to update query based on the event argument.  In uses partial matching of event names.  Decided to use this as an internal function.

--- R/top_times.R
+++ R/top_times.R
@@ -1,14 +1,17 @@
+#' Prepare SQL Query
 #'
+#' The function prepares a SQL query based on the parameters provided.  
 #'
+#' @param conference a character vector with the name of an NCAA swimming conference.  If not provided the results include all conferences.  
+#' @param team_name A character vector with the name of an NCAA swimming conference.  If not provided the results include all teams  
+#' @param team_name a character vector with the team code of an NCAA swimming team  If not provided the results include all teams  
+#' @param invidivdual a character vector with the name or athlete id of a swimmier.  If not provided the results include all swimmers.  The athlete id follows the USA Swimming standard.
+#' @param event a character vector with the name of swimming envent.  If not provided the results include all events.  
 #'
-#' @param team - 
-#'
-#' 
-#' TO DO:  
-#'  * Adjust Cases of Filters
+#' @keyword internal
+#'  
 
-top_times <- function(db_path, 
-					  conference = NULL, 
+query_prep <- function(conference = NULL, 
 					  team_name = NULL,
 					  team_code = NULL, 
 					  individual = NULL, 
@@ -101,6 +104,7 @@ top_times <- function(db_path,
 					)
 		print(names)
 		print(id)
+		
 		### Prepare Where Clasue
 		if(length(names) >= 1 & length(id) >= 1){
 			athlete_string <- paste("AND ATHLETE_NAME In ('",
@@ -109,7 +113,7 @@ top_times <- function(db_path,
 									" OR ATHLETE_CODE In('",
 									paste(id, collapse = "', '"), 
 									"')",
-									sep = "")
+								sep = "")
 
 			prepared_query <- sub('ATHELTE_FILLER', 
 								  athlete_string, 
@@ -119,7 +123,7 @@ top_times <- function(db_path,
 			athlete_string <- paste("AND ATHLETE_NAME In ('",
 			 						paste(names, collapse = "', '"), 
 			 						"')",
-									sep = "")
+								sep = "")
 			prepared_query <- sub('ATHELTE_FILLER', 
 								   athlete_string, 
 								   prepared_query)	
@@ -128,15 +132,36 @@ top_times <- function(db_path,
 			athlete_string <- paste("AND ATHLETE_ID In ('",
 			 						paste(names, collapse = "', '"), 
 			 						"')",
-									sep = "")
+								sep = "")
 			prepared_query <- sub('ATHELTE_FILLER', 
 								   athlete_string, 
 								   prepared_query)	
 		}
 	}
 
+	## Events
+	if (event == 'All') {
+		prepared_query <- sub('EVENT_FILLER', "", prepared_query)
+
+	} else {
+	### Prepare Events so they match event names in the DB
+		event2 <- c()
+		for (i in event) {
+			m <- match.arg(i, unique(D2Qualifying$event))
+			event2 <- append(event2, m)
+		}
+
+		event_string <- paste('AND EVENT IN (', 
+							  "'", 
+							  paste(event2, collapse = "', '"), 
+							  "')'", 
+					 sep = "")
+
+		prepared_query <- sub('EVENT_FILLER', event_string, prepared_query)
+	}
+
 	## Final Query Prep
-	prepared_query <- sub('WHERE\\s AND', "WHERE", prepared_query)
+	prepared_query <- sub('WHERE\\s+ AND', "WHERE", prepared_query)
 
 	return(prepared_query)
 }
